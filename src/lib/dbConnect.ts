aac65a66c8919a08f4b1f71055d83bec25ee4b40
Muhammad Abdullah <rana.abdullah3655@gmail.com>
2024-06-23 22:45:20 +0500

    code got optimized

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -7,22 +7,28 @@ type ConnectionObject = {
 const connection: ConnectionObject = {};
 
 async function dbConnect(): Promise<void> {
+  // if (!process.env.MONGO_DB_URI) {
+  //   console.log("MongoDB URI not found in environment variables");
+  //   return;
+  // }
   if (connection.isConnected) {
     console.log("Already connected to database");
     return;
   }
   try {
-    const db = await mongoose.connect(process.env.MONGO_DB_URI || "", {});
+    const db = await mongoose.connect(
+      "mongodb+srv://abdullah1816:<mongodb1816>@cluster0.hmixa7q.mongodb.net/mistery-message",
+      {}
+    );
 
-    console.log(db)
+    console.log(db);
     connection.isConnected = db.connections[0].readyState;
 
-    console.log('DB Connected Successfully')
+    console.log("DB Connected Successfully");
   } catch (error) {
-    console.log("Database connection failed",error)
-    process.exit(1)
+    console.log("Database connection failed", error);
+    process.exit(1);
   }
 }
 
-
-export default dbConnect
+export default dbConnect;
--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,4 +1,3 @@
-import { Resend } from 'resend';
+import { Resend } from "resend";
 
-export  const resend = new Resend(process.env.RESEND_API_KEY);
-
+export const resend = new Resend("re_T1nWSfdb_3aDTToKUnQHda2bb7v8kfc1k");
