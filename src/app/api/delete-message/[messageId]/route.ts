f0a772f5619855f678adb2a7a45e1bb338cd319b
Muhammad Abdullah <rana.abdullah3655@gmail.com>
2024-08-06 02:20:59 +0500

    Delete Api created

--- src/app/api/delete-message/[messageId]/route.ts
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -1,6 +1,44 @@
-import { ApiResponse } from "@/types/ApiResponse";
-import axios from "axios";
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import UserModel from "@/model/User";
 
- export async function DELETE(messageId: string) {
-  return axios.delete<ApiResponse>(`/api/delete-message/${messageId}`)
- }
+export async function DELETE(
+  request: Request,
+  { params }: { params: { messageId: string } }
+) {
+  const messageId = params.messageId;
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+  if (!session || !session.user) {
+    return Response.json(
+      { sucess: false, message: "Not Authenticated" },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const updateResult = await UserModel.updateOne(
+      { _id: user.id },
+      { $pull: { messages: { _id: messageId } } }
+    );
+
+    if (updateResult.modifiedCount == 0) {
+      return Response.json(
+        { sucess: false, message: "Message not found or already deleted" },
+        { status: 404 }
+      );
+    }
+    return Response.json(
+      { sucess: true, message: "Message deleted" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error in Delete message route", error);
+    return Response.json(
+      { sucess: false, message: "Error deleting message" },
+      { status: 500 }
+    );
+  }
+}
